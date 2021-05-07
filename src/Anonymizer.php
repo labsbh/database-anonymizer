21aa5588cb2f23c3ee9b38cb7915032d9bb203f0
Pascal Montoya <pascal.montoya@gmail.com>
2021-05-07 15:47:31 +0200

    :ambulance: Fix row by row update

--- src/Anonymizer.php
+++ src/Anonymizer.php
@@ -1,9 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace WebnetFr\DatabaseAnonymizer;
 
 use WebnetFr\DatabaseAnonymizer\Event\AnonymizerEvent;
 use Doctrine\DBAL\Connection;
+use WebnetFr\DatabaseAnonymizer\Exception\InvalidAnonymousValueException;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 /**
@@ -13,6 +16,8 @@ use Symfony\Component\EventDispatcher\EventDispatcherInterface;
  */
 class Anonymizer
 {
+    private ?EventDispatcherInterface $dispatcher;
+
     public function __construct(?EventDispatcherInterface $dispatcher = null)
     {
         $this->dispatcher = $dispatcher;
@@ -21,33 +26,59 @@ class Anonymizer
     /**
      * Anonymize entire database based on target tables.
      *
-     * @param Connection $connection
+     * @param Connection    $connection
      * @param TargetTable[] $targets
      *
-     * @throws \Doctrine\DBAL\DBALException
+     * @throws \Doctrine\DBAL\Driver\Exception
+     * @throws \Doctrine\DBAL\Exception
+     * @throws \Exception
      */
-    public function anonymize(Connection $connection, array $targets)
+    public function anonymize(Connection $connection, array $targets): void
     {
         foreach ($targets as $targetTable) {
             if ($targetTable->isTruncate()) {
                 $dbPlatform = $connection->getDatabasePlatform();
-                $connection->query('SET FOREIGN_KEY_CHECKS=0');
+                if (null === $dbPlatform) {
+                    throw new \Exception('Db platform should not be null');
+                }
+                $connection->executeQuery('SET FOREIGN_KEY_CHECKS=0');
                 $truncateQuery = $dbPlatform->getTruncateTableSql($targetTable->getName());
-                $connection->executeUpdate($truncateQuery);
-                $connection->query('SET FOREIGN_KEY_CHECKS=1');
+                $connection->executeStatement($truncateQuery);
+                $connection->executeQuery('SET FOREIGN_KEY_CHECKS=1');
             } else {
+                $allFieldNames = $targetTable->getAllFieldNames();
+                $pk            = $targetTable->getPrimaryKey();
+
+                $fetchRowsSQL  = $connection
+                    ->createQueryBuilder()
+                    ->select(implode(',', $allFieldNames))
+                    ->from($targetTable->getName())
+                    ->getSQL();
+                $fetchRowsStmt = $connection->prepare($fetchRowsSQL);
+                $result        = $fetchRowsStmt->executeQuery();
+
                 // Anonymize all rows in current target table.
-                $values = [];
-                foreach ($targetTable->getTargetFields() as $targetField) {
-                    if (!isset($this->fakerCache[$targetField->getName()])) {
-                        $this->fakerCache[$targetField->getName()] = $targetField->generate();
+                while ($row = $result->fetchAssociative()) {
+                    $values = [];
+                    // Anonymize all target fields in current row.
+                    foreach ($targetTable->getTargetFields() as $targetField) {
+                        $anonValue = $targetField->generate();
+
+                        if (null !== $anonValue && !\is_string($anonValue)) {
+                            throw new InvalidAnonymousValueException('Generated value must be null or string');
+                        }
+
+                        // Set anonymized value.
+                        $values[$targetField->getName()] = $anonValue;
                     }
 
-                    // Set anonymized value.
-                    $values[$targetField->getName()] = $this->fakerCache[$targetField->getName()];
-                }
+                    $pkValues = [];
+                    foreach ($pk as $pkField) {
+                        $pkValues[$pkField] = $row[$pkField];
+                    }
 
-                $connection->update($targetTable->getName(), $values, [true => true]);
+                    $connection->update($targetTable->getName(), $values, $pkValues);
+                }
 
                 if (null !== $this->dispatcher) {
                     $this->dispatcher->dispatch(new AnonymizerEvent($targetTable->getName(), $values));
